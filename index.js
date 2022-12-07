02995dcdfadcc384ba058c5fcbf24b4d7557d569
erikphinguyen <erikphinguyen@gmail.com>
2022-12-07 09:56:43 -0800

    started kitchen-frontend

---
+++ index.js
@@ -0,0 +1,11 @@
+// <!-- <!DOCTYPE html>
+//     <div class="orders-container">
+//         <form onsubmit="some-fx">
+//             <input value="order.name">
+//             <input value="order.address">
+//             <input value="order.items">
+//             <button type="submit">Submit</button>
+//         </form>
+//     </div> -->
+
+
