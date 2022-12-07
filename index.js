f9049b1bbec172fd5cbb90fb760776f32c583562
erikphinguyen <erikphinguyen@gmail.com>
2022-12-07 10:26:25 -0800

    vanilla js done

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
+/*
 const render = () => {
     const root = document.getElementById("root");
     const App = document.createElement("div")
 
     App.innerText = "Hello World w/ JS";
 
-    root.appendChild(App);
+    const OrderForm = document.createElement("Form");
+
+    const name = document.createElement("input");
+    name.placeholder = "Name";
+    const address = document.createElement("input");
+    address.placeholder = "Address";
+    const items = document.createElement("input");
+    items.placeholder = "Items";
+
+    const submit = document.createElement("button");
+    submit.innerText = "Submit"
+
+    OrderForm.append(name, address, items, submit);
+    App.append(OrderForm)
+    root.append(App);
 }
+*/
 
 document.addEventListener('DOMContentLoaded', () => {
     render();
 })
+
+// WHAT REACTS BUILDS HTML FOR US -- building HTML is basically editting nodes in a tree
+const Form = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
